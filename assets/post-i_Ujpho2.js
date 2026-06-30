282d2810bea09272d6434c57612b3739ad68001a
Mr.Rain <editsrepresas@gmail.com>
2026-06-30 17:10:07 +0800

    Updates

--- assets/post-i_Ujpho2.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./index-i4WHy1vn.js";var t=()=>e.get(`/posts`),n=()=>e.get(`/posts/options`),r=t=>e.post(`/posts`,t),i=(t,n)=>e.put(`/posts/${t}`,n),a=t=>e.delete(`/posts/${t}`);export{i as a,n as i,a as n,t as r,r as t};
